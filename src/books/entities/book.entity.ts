cb241ec9ab64d557517c30167d67fb19c39c00a6
Tam Dao <>
2022-11-05 16:31:19 +0700

    Update book entity to add some fields follow design

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,28 @@
-export class Book {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  genre: string;
+
+  @Column()
+  author: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
